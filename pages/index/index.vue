a522e3ea5f458213062fb2ded582163b8ac35a64
luoliang126 <394500842@qq.com>
2021-01-08 11:29:49 +0800

    新增微信小程序内容

---
+++ pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+
+		<view class="uni-list">
+			<view class="uni-list-cell">
+				<view class="uni-list-cell-left">
+					当前选择
+				</view>
+				<view class="uni-list-cell-db">
+					<picker mode="time" :value="time" start="09:01" end="21:01" @change="bindTimeChange">
+						<view class="uni-input">{{ time }}</view>
+					</picker>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				time:''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			// 时间切换
+			bindTimeChange(val){
+				console.log(val);
+				this.time = val.detail.value;
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		这是登录界面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
